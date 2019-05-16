be00bd0f025a3e698e0ecc20801dfb09d030af5f
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2019-05-16 11:45:33 +0300

    Add project files.

---
+++ Ultijer/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ultijer
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		Grid home, movies;
+		bool removed_home = false;
+		public MainWindow()
+		{
+			InitializeComponent();
+			home = Home;
+			parent.Children.Remove(this.home); removed_home = true;
+		}
+		public void OnButtonClick(object sender, RoutedEventArgs e) {
+			if (j_n.Content.Equals("Journal")) {
+				j_n.Content = "Notes";
+			}
+			else if (j_n.Content.Equals("Notes"))
+			{
+				j_n.Content = "Journal";
+			}
+			if (removed_home)
+			{
+				parent.Children.Add(home);
+				removed_home = false;
+			}
+			else {
+				parent.Children.Remove(home);
+				removed_home = true;
+			}
+		}
+	}
+}
