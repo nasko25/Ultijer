fbc841daf72e49ff58275348ebd5ca5c394273ef
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2019-06-27 12:40:31 +0300

    implemented smoother scrolling

---
+++ Ultijer/PixelBasedScrollingBehavior.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+// NOT NEEDED 
+// DID NOT WORK FOR .NET 4.0 (https://stackoverflow.com/questions/1977929/wpf-listbox-with-a-listbox-ui-virtualization-and-scrolling/9875475#9875475)
+// SO CHANGED THE VERSION TO .NET 4.5
+
+namespace Ultijer
+{
+    public static class PixelBasedScrollingBehavior
+    {
+		public static bool GetIsEnabled(DependencyObject obj)
+		{
+			return (bool)obj.GetValue(IsEnabledProperty);
+		}
+
+		public static void SetIsEnabled(DependencyObject obj, bool value)
+		{
+			obj.SetValue(IsEnabledProperty, value);
+		}
+
+		public static readonly DependencyProperty IsEnabledProperty =
+			DependencyProperty.RegisterAttached("IsEnabled", typeof(bool), typeof(PixelBasedScrollingBehavior), new UIPropertyMetadata(false, HandleIsEnabledChanged));
+
+		private static void HandleIsEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var vsp = d as VirtualizingStackPanel;
+			if (vsp == null)
+			{
+				return;
+			}
+
+			var property = typeof(VirtualizingStackPanel).GetProperty("IsPixelBased",
+																		 BindingFlags.NonPublic | BindingFlags.Instance);
+
+			if (property == null)
+			{
+				throw new InvalidOperationException("Pixel-based scrolling behaviour hack no longer works!");
+			}
+
+			if ((bool)e.NewValue == true)
+			{
+				property.SetValue(vsp, true, new object[0]);
+			}
+			else
+			{
+				property.SetValue(vsp, false, new object[0]);
+			}
+		}
+	}
+}
