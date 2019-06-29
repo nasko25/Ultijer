8de4822f15bef34b024d902cbff19eb3f7969634
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2019-06-29 18:59:46 +0300

    fixed how the buttons work

--- Ultijer/MainWindow.xaml.cs
+++ Ultijer/MainWindow.xaml.cs
@@ -17,6 +17,9 @@ namespace Ultijer
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
+	/// 
+
+	// TODO probably the OnButtonClick(..., ...) should be implemented with a switch statement  !!!!
 	public partial class MainWindow : Window
 	{
 		Grid home, movies;
@@ -30,27 +33,39 @@ namespace Ultijer
 			parent.Children.Remove(this.movies); removed_movies = true;
 		}
 		public void OnButtonClick(object sender, RoutedEventArgs e) {
-			if (j_n.Content.Equals("Journal")) {
+			String sender_string = sender.ToString().Substring(sender.ToString().IndexOf(":") + 2); // 2. because there is : and a space too much
+			if (j_n.Content.Equals("Journal") && sender_string.Equals("Journal")) {
 				j_n.Content = "Notes";
 			}
-			else if (j_n.Content.Equals("Notes"))
+			else if (j_n.Content.Equals("Notes") && sender_string.Equals("Notes"))
 			{
 				j_n.Content = "Journal";
 			}
-			if (removed_home)
+			if (removed_home && sender.Equals(home_button))
 			{
 				parent.Children.Add(home);
 				removed_home = false;
 			}
-			else {
+			else if (!removed_home && !sender.Equals(home_button)) {
 				parent.Children.Remove(home);
 				removed_home = true;
 			}
-			if (sender.Equals(movies_button) && removed_movies) {
+			if (sender.Equals(movies_button) && removed_movies)
+			{
 				parent.Children.Add(movies);
 				removed_movies = false;
 				MessageBox.Show("YOU SELECTED THE MOVIES BUTTON");
 			}
+			else if (!sender.Equals(movies_button) && !removed_movies) {
+				parent.Children.Remove(movies);
+				removed_movies = true;
+			}
+			// TODO instead of remopving the elements in the view one by one, try to remove them all together like:
+			/* foreach (System.Windows.UIElement i in parent.Children) {
+				parent.Children.Remove(i);
+			}
+			in all cases when a button is pressed, and parent.Children is not null.
+			*/ 
 		} 
 	}
 }
