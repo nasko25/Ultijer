be70f599c8e257f91ec770d30cae1ef1f0d99a53
DESKTOP-MO7QTPP\Atanas Pashov <atanaspashov19@gmail.com>
2019-08-01 16:34:10 +0300

    finished the MS tutorial on animations

--- Ultijer/Pages/Books.xaml.cs
+++ Ultijer/Pages/Books.xaml.cs
@@ -20,18 +20,30 @@ namespace Ultijer.Pages
 	/// </summary>
 	public partial class Books : Page
 	{
+		private Storyboard storyboard;
 		public Books()
 		{
 			InitializeComponent();
-		}
 
-		public void OnClick(object sender, RoutedEventArgs e) {
 			var myDoubleAnimation = new DoubleAnimation
 			{
 				From = 1.0,
 				To = 0.0,
 				Duration = new Duration(TimeSpan.FromSeconds(5))
-		};
+			};
+
+			storyboard = new Storyboard();
+			storyboard.Children.Add(myDoubleAnimation);
+			Storyboard.SetTargetName(myDoubleAnimation, MyRectangle.Name);
+			/*
+			 * For more information about TargetProperty syntax and for additional examples, see the Storyboards Overview.
+			 * https://docs.microsoft.com/en-us/dotnet/framework/wpf/graphics-multimedia/storyboards-overview
+			 */
+			Storyboard.SetTargetProperty(myDoubleAnimation, new PropertyPath(Rectangle.OpacityProperty));
+		}
+
+		public void OnClick(object sender, RoutedEventArgs e) {
+			storyboard.Begin(this);
 		}
 	}
 }
